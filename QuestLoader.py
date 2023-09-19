6989434eec1281fd01afec4fff8f08199466e5ca
Keiron Beadle <kbeadle@climaxstudios.com>
2023-09-19 11:56:04 +0100

    Initial push

---
+++ QuestLoader.py
@@ -0,0 +1,98 @@
+import os, json, glob
+
+#Defines
+quests = []
+monsterMap = {
+    "boar":1,
+    "suzy":2
+}
+locationMap={
+    "greatrock":1,
+    "daisywoods":2
+}
+itemMap={
+    "boarmeat":1
+}
+npcMap={
+    "borris":1,
+    "chalice":2,
+    "moo":3
+}
+giverQuestIdMap={
+}
+for npc in npcMap.keys():
+    giverQuestIdMap[npc] = -1
+#end defines
+
+#Quest classes
+class Slay:
+    def __init__(self, name,giver : str,description,monster : str,count, minLevel, requires):
+        self.Name = name
+        self.GiverId = npcMap[giver.lower()]
+        self.GiverQuestId = giverQuestIdMap[giver]
+        self.Description = description
+        self.MonsterId = monsterMap[monster.lower()]
+        self.Count = count
+        self.MinLevel = minLevel
+        self.Requires = requires
+
+class Provide:
+    def __init__(self, name, giver : str, description, item : str, count, minLevel, requires):
+        self.Name = name
+        self.GiverId = npcMap[giver.lower()]
+        self.GiverQuestId = giverQuestIdMap[giver]
+        self.Description = description
+        self.ItemId = itemMap[item.lower()]
+        self.Count = count
+        self.MinLevel = minLevel
+        self.Requires = requires
+
+class Travel:
+    def __init__(self, name, giver : str, description, location : str, minLevel, requires):
+        self.Name = name
+        self.GiverId = npcMap[giver.lower()]
+        self.GiverQuestId = giverQuestIdMap[giver]
+        self.Description = description
+        self.LocationId = locationMap[location.lower()]
+        self.MinLevel = minLevel
+        self.Requires = requires
+#end quest classes
+
+# begin fill quests
+jsonFiles = glob.glob("*.json")
+for file in jsonFiles:
+    fr = open(file, 'r')
+    giver = file.removesuffix("Quests.json").lower()
+    jsonData = json.loads(fr.read())
+    for quest in jsonData["Quests"]:
+        giverQuestIdMap[giver] += 1
+        if quest["Type"] == "Slay":
+            quests.append(Slay(quest["Name"],giver,quest["Description"]
+                               ,quest["Monster"],quest["Count"], quest["MinLevel"], quest["Requires"]))
+        elif quest["Type"] == "Provide":
+            quests.append(Provide(quest["Name"],giver,quest["Description"]
+                                  ,quest["Item"],quest["Count"], quest["MinLevel"], quest["Requires"]))
+        elif quest["Type"] == "Travel":
+            quests.append(Travel(quest["Name"],giver,quest["Description"]
+                                 ,quest["Location"], quest["MinLevel"], quest["Requires"]))
+    fr.close()
+
+# end fill quests
+
+# begin create verse
+
+with open('QuestLoader.verse','w') as f:
+    f.write("quest_loader := class{var Quests<public> : []quest = array{};GetQuests<public>():[]quest={return Quests};Init<public>():void={")
+    for quest in quests:
+        f.write("set Quests += array{")
+        if isinstance(quest, Slay):
+            f.write(f'''MakeBKSlayQuest("{quest.Name}",{quest.GiverId},{quest.GiverQuestId},"{quest.Description}",{quest.MonsterId},{quest.Count},{quest.MinLevel},"{quest.Requires}")''')
+        elif isinstance(quest, Provide):
+            f.write(f'''MakeBKProvideQuest("{quest.Name}",{quest.GiverId},{quest.GiverQuestId},"{quest.Description}",{quest.ItemId},{quest.Count},{quest.MinLevel},"{quest.Requires}")''')
+        elif isinstance(quest, Travel):
+            f.write(f'''MakeBKTravelQuest("{quest.Name}",{quest.GiverId},{quest.GiverQuestId},"{quest.Description}",{quest.LocationId},{quest.MinLevel},"{quest.Requires}")''')
+        f.write("};")
+    f.write('}')
+    f.write('}')
+
+# end create verse
